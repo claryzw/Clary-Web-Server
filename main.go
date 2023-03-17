002e2f3a56a7463b36093955ede5b30b6aca78f4
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-03-17 21:26:23 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,17 @@
+// Clarence Itai Msindo's Web Server
+package main
+
+//The imports for the web server
+import "fmt"
+import "net/http"
+
+// The functions for the web server
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
