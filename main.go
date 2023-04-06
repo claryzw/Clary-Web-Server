c117f2e4a55175c250e39f2ce1bbc90002e96b68
Clarence Itai <61044722+claryzw@users.noreply.github.com>
2023-04-06 21:17:12 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import "net/http"
 // The functions for the web server
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
+		fmt.Fprint(w, "This is a Simple Web Server from Clarence Itai Msindo!")
 	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
